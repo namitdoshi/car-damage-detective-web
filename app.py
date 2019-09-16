b199a140e1211eb9b7db6a735c9f322027a486cc
Namit Doshi <namitdoshi90@gmail.com>
2019-09-16 12:32:42 +0530

    signup page route added in application

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
+@app.route('/signup')
+def signup():
+  return render_template('signup.html')
+
 
 if __name__ == '__main__':
   app.run(debug=True)
