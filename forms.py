fc76651668fa0bf33210edc422c828d685a287b3
Namit Doshi <namitdoshi90@gmail.com>
2019-10-13 19:19:55 +0530

    minor changes

--- forms.py
+++ forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email
+from flask_wtf.file import FileField, FileAllowed
+
 
 class RegisterationForm(FlaskForm):
   username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
@@ -12,3 +14,6 @@ class LoginForm(FlaskForm):
   email = StringField('Email', validators=[DataRequired(), Email()])
   password = PasswordField('Password', validators=[DataRequired()])
   submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+  picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
