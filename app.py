a9e28c19b80ab5702ecbeaed833347d28783722d
Namit Doshi <namitdoshi90@gmail.com>
2019-10-13 16:52:20 +0530

    some changes with forms

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
+from forms import RegisterationForm, LoginForm
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '1747afcc43886f257ed0860ec3bc98fd'
+
 @app.route('/')
 def index():
-  return render_template('index.html')
+  form = LoginForm()
+  return render_template('index.html', form=form)
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-  return render_template('signup.html')
+  form = RegisterationForm()
+  if form.validate_on_submit():
+    flash(f'Account created for {form.email.data}!', 'success')
+    return redirect(url_for('/'))
+  else:
+    print('Kataa')
+  return render_template('signup.html', title='Register', form=form)
 
 
 if __name__ == '__main__':
